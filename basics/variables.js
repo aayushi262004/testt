1f42732b8c2270ec331bd36ce25789c6a534cb76
Aayushi Gupta <aayushi262004@gmail.com>
2025-03-26 19:52:48 +0000

    datatypes

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+accountCity = "Jaipur"
+var accountPassword="12345"
+
+//accountId = 2 //not allowed
+
+accountEmail="hc@hc.com"
+ccountPassword="212121"
+accountCity="Bengaluru"
+
+console.log(accountId);
+console.table([accountId]);
